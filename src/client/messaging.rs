bdbb4e5cd3b869173eab891a4248f53a78c77622
m10090 <tammwy22@gmail.com>
2024-08-08 07:55:23 +0300

    feature: impl ServerFuncBuf

--- src/client/messaging.rs
+++ src/client/messaging.rs
@@ -12,25 +12,24 @@ pub async fn client_send_message(msg: Message) -> Result<(), Error> {
         writer_stream.send(msg).await?;
         return Ok(());
     }
-
-    return Err(Error::Io(std::io::Error::new(
+    Err(Error::Io(std::io::Error::new(
         std::io::ErrorKind::NotConnected,
         "WebSocket stream not initialized",
-    )));
+    )))
 }
 /// create a new on message handler that will handle incoming message
 /// takin the consderation the message is received or not
+#[inline]
 pub(super) fn get_on_message(mut reader: ReaderWsStream) -> impl Future<Output = ()> {
     async move {
         while let Some(message) = reader.next().await {
             let message = message.expect("Failed to get message"); // todo: handle error
             dbg!(&message);
 
-            // #[cfg(feature = "integration_testing_client")]
+            #[cfg(feature = "integration_testing_client")]
             tokio::spawn(crate::integration_testing::reseived_message(message));
 
             // todo!("Handle message: {:?}", message);
-            // function for testing
         }
     }
 }
