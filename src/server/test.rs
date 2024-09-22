8b67ff1d829679d8a193151ccb79450dd9de573b
Mohamed El tamawey <tammwy22@gmail.com>
2024-09-22 05:16:40 +0300

    test: improve the tests

--- src/server/test.rs
+++ src/server/test.rs
@@ -1,132 +1,167 @@
 use super::*;
-use futures::future::{BoxFuture, FutureExt};
 use serial_test::serial;
-use std::error::Error;
 use std::io;
-use std::thread::sleep;
-use std::time::Duration;
 use tokio::fs;
+use tokio::time::{sleep, Duration};
 
-#[tokio::test]
-#[serial]
-async fn watcher_rename_file() {
-    let res = async {
-        fs::File::create_new("test.txt").await?;
-        tokio::spawn(watch_file_change());
-        fs::rename("test.txt", "test2.txt").await?;
-        
-        let api = API.lock().await;
-        let (files, _) = api.get_file_maps().await;
-        files
-            .binary_search(&"./test2.txt".to_string())
-            .map_err(|_| io::Error::new(io::ErrorKind::NotFound, ""))?;
-        drop(api);
-        Ok::<_, io::Error>(())
-    }
-    .await;
-    let _ = fs::remove_file("test2.txt").await;
-    let _ = fs::remove_file("test.txt").await;
-    res.unwrap();
+lazy_static! {
+    static ref RT: tokio::runtime::Runtime = tokio::runtime::Runtime::new().unwrap();
+}
+use ctor::ctor;
+#[ctor]
+fn start_watch() {
+    std::thread::spawn(|| {
+        let rt = tokio::runtime::Runtime::new().unwrap();
+        rt.block_on(watch_file_change());
+    });
+    std::thread::spawn(|| {
+        let rt = tokio::runtime::Runtime::new().unwrap();
+        rt.block_on(start_server(8080));
+    });
+    std::thread::sleep(std::time::Duration::from_secs(2));
 }
 
-#[tokio::test]
+#[test]
 #[serial]
-async fn watcher_rename_dir() {
-    let res = async {
-        fs::create_dir("emty_dir").await?;
-        tokio::spawn(watch_file_change());
-        fs::rename("emty_dir", "emty_dir2").await?;
+fn watcher_rename_file() {
+    RT.block_on(async {
+        let res = async {
+            fs::File::create_new("test.txt").await?;
 
-        sleep(Duration::from_secs(2));
+            fs::rename("test.txt", "test2.txt").await?;
+            sleep(Duration::from_secs(5)).await;
 
-        let api = API.lock().await;
-        let (_, emty_dir) = api.get_file_maps().await;
-        emty_dir
-            .binary_search(&"./emty_dir2/".to_string())
-            .map_err(|_| io::Error::new(io::ErrorKind::NotFound, ""))?;
-        Ok::<_, io::Error>(())
-    }
-    .await;
-    let _ = fs::remove_dir("emty_dir2").await;
-    let _ = fs::remove_dir("emty_dir").await;
-    res.unwrap();
+            let api = API.lock().await;
+            let (files, _) = api.get_file_maps().await;
+            files
+                .binary_search(&"./test2.txt".to_string())
+                .map_err(|_| io::Error::new(io::ErrorKind::NotFound, ""))?;
+            drop(api);
+            Ok::<_, io::Error>(())
+        }
+        .await;
+        let _ = fs::remove_file("test2.txt").await;
+        let _ = fs::remove_file("test.txt").await;
+        res
+    })
+    .unwrap();
+}
+
+#[test]
+#[serial]
+fn watcher_rename_dir() {
+    RT.block_on(async {
+        let res = async {
+            fs::create_dir("emty_dir").await?;
+
+            fs::rename("emty_dir", "emty_dir2").await?;
+
+            sleep(Duration::from_secs(5)).await;
+
+            let api = API.lock().await;
+            let (_, emty_dir) = api.get_file_maps().await;
+            emty_dir
+                .binary_search(&"./emty_dir2/".to_string())
+                .map_err(|_| io::Error::new(io::ErrorKind::NotFound, ""))?;
+            Ok::<_, io::Error>(())
+        }
+        .await;
+        let _ = fs::remove_dir("emty_dir2").await;
+        let _ = fs::remove_dir("emty_dir").await;
+        res
+    })
+    .unwrap();
 }
 //
-#[tokio::test]
-#[serial]
-async fn watcher_create_file() {
-    let res = async {
-        tokio::spawn(watch_file_change());
-        fs::File::create_new("text.txt").await?;
 
-        let api = API.lock().await;
-        let (files, _) = api.get_file_maps().await;
-        files
-            .binary_search(&"./text.txt".to_string())
-            .map_err(|_| io::Error::new(io::ErrorKind::NotFound, ""))?;
-        Ok::<_, io::Error>(())
-    }
-    .await;
-    let _ = fs::remove_file("text.txt").await;
-    res.unwrap();
+#[test]
+#[serial]
+fn watcher_create_file() {
+    RT.block_on(async {
+        let res = async {
+            sleep(Duration::from_secs(2)).await;
+            fs::File::create_new("text.txt").await?;
+            sleep(Duration::from_secs(5)).await;
+            let api = API.lock().await;
+            let (files, _) = api.get_file_maps().await;
+            files
+                .binary_search(&"./text.txt".to_string())
+                .map_err(|_| io::Error::new(io::ErrorKind::NotFound, ""))?;
+            Ok::<_, io::Error>(())
+        }
+        .await;
+        let _ = fs::remove_file("text.txt").await;
+        res
+    })
+    .unwrap();
 }
 
-#[tokio::test]
+#[test]
 #[serial]
-async fn watcher_create_dir() {
-    let res = async {
-        tokio::spawn(watch_file_change());
-        fs::create_dir("emty_dir").await?;
+fn watcher_create_dir() {
+    RT.block_on(async {
+        let res = async {
+            sleep(Duration::from_secs(2)).await;
+            fs::create_dir("emty_dir").await?;
+            sleep(Duration::from_secs(5)).await;
 
-        let api = API.lock().await;
-        let (_, dirs) = api.get_file_maps().await;
-        dirs.binary_search(&"./emty_dir/".to_string()).unwrap();
-        Ok::<_, io::Error>(())
-    }
-    .await;
-    let _ = fs::remove_dir("emty_dir").await;
-    res.unwrap();
+            let api = API.lock().await;
+            let (_, dirs) = api.get_file_maps().await;
+            dirs.binary_search(&"./emty_dir/".to_string()).unwrap();
+            Ok::<_, io::Error>(())
+        }
+        .await;
+        let _ = fs::remove_dir("emty_dir").await;
+        res
+    })
+    .unwrap();
 }
 
-#[tokio::test]
+#[test]
 #[serial]
-async fn watcher_delete_file() {
-    let res = async {
-        fs::File::create_new("text.txt").await.unwrap();
-        sleep(Duration::from_secs(2));
-        tokio::spawn(watch_file_change());
-        sleep(Duration::from_secs(2));
-        fs::remove_file("text.txt").await.unwrap();
-        sleep(Duration::from_secs(2));
+fn watcher_delete_file() {
+    RT.block_on(async {
+        let res = async {
+            fs::File::create_new("text.txt").await.unwrap();
 
-        let api = API.lock().await;
-        let (files, _) = api.get_file_maps().await;
-        if files.binary_search(&"./text.txt".to_string()).is_ok() {
-            return Err(io::Error::new(io::ErrorKind::NotFound, ""));
-        };
-        Ok::<_, io::Error>(())
-    }
-    .await;
-    let _ = fs::remove_file("text.txt").await;
-    res.unwrap();
+            sleep(Duration::from_secs(2)).await;
+            fs::remove_file("text.txt").await.unwrap();
+            sleep(Duration::from_secs(5)).await;
+
+            let api = API.lock().await;
+            let (files, _) = api.get_file_maps().await;
+            if files.binary_search(&"./text.txt".to_string()).is_ok() {
+                return Err(io::Error::new(io::ErrorKind::NotFound, ""));
+            };
+            Ok::<_, io::Error>(())
+        }
+        .await;
+        let _ = fs::remove_file("text.txt").await;
+        res
+    })
+    .unwrap();
 }
 
-#[tokio::test]
+#[test]
 #[serial]
-async fn watcher_delete_dir() {
-    let res = async {
-        fs::create_dir("emty_dir").await?;
-        tokio::spawn(watch_file_change());
-        fs::remove_dir("emty_dir").await?;
+fn watcher_delete_dir() {
+    RT.block_on(async {
+        let res = async {
+            fs::create_dir("emty_dir").await?;
 
-        let api = API.lock().await;
-        let (_, dirs) = api.get_file_maps().await;
-        if dirs.binary_search(&"./emty_dir/".to_string()).is_ok() {
-            return Err(io::Error::new(io::ErrorKind::NotFound, ""));
-        };
-        Ok::<_, io::Error>(())
-    }
-    .await;
-    let _ = fs::remove_dir("emty_dir").await;
-    res.unwrap();
+            sleep(Duration::from_secs(2)).await;
+            fs::remove_dir("emty_dir").await?;
+            sleep(Duration::from_secs(3)).await;
+            let api = API.lock().await;
+            let (_, dirs) = api.get_file_maps().await;
+            if dirs.binary_search(&"./emty_dir/".to_string()).is_ok() {
+                return Err(io::Error::new(io::ErrorKind::NotFound, ""));
+            };
+            Ok::<_, io::Error>(())
+        }
+        .await;
+        let _ = fs::remove_dir("emty_dir").await;
+        res
+    })
+    .unwrap();
 }
